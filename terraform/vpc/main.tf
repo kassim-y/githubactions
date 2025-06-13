fd9af92e3881875969881de05d468c74239608b9
kassim-y <yussifkassim8@gmail.com>
2025-06-12 20:04:10 -0400

    Create main.tf

---
+++ terraform/vpc/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "REPLACE_WITH_UNIQUE_BUCKET_NAME"  # Use a globally unique name
+    key            = "vpcactions/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "github-actions-vpc"
+  }
+}
